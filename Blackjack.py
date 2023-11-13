fdda788153f93bc31f44cec89dd6779dcad0c11e
awinkhater <112085352+awinkhater@users.noreply.github.com>
2023-11-13 18:08:04 -0500

    Update Blackjack.py
    Added a shuffle and deal function

--- Blackjack.py
+++ Blackjack.py
@@ -1,11 +1,20 @@
 import numpy as np
+import random
 
-Hearts=[2,3,4,5,6,7,8,9,10,11,12,13,'A']
+
+
+Hearts=[2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 Clubs=Hearts.copy()
 Spades=Hearts.copy()
 Diamonds=Hearts.copy()
-
 Deck=Hearts+Clubs+Spades+Diamonds
 
+def shuffle_deal(deck, n):
+    d=[]
+    pile=[]
+    random.shuffle(deck)
+    for i in range(0,n):
+        pile.append(deck[i])
+    return (pile)
 
-
+print(shuffle_deal(Deck,2))
