f2b706e3f9468456637537e2ab86f89ad2b17cf8
awinkhater <112085352+awinkhater@users.noreply.github.com>
2023-11-13 18:39:54 -0500

    Update Blackjack.py
    Fixed the function

--- Blackjack.py
+++ Blackjack.py
@@ -2,19 +2,22 @@ import numpy as np
 import random
 
 
-
+#The base Deck Suits Dont matter for the game
 Hearts=[2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 Clubs=Hearts.copy()
 Spades=Hearts.copy()
 Diamonds=Hearts.copy()
 Deck=Hearts+Clubs+Spades+Diamonds
 
+
+#This function deals n cards and from the deck and removes them
+#It returns a tuple of (the deck with the dealt cards removed, the n dealt cards)
 def shuffle_deal(deck, n):
-    d=[]
     pile=[]
     random.shuffle(deck)
     for i in range(0,n):
-        pile.append(deck[i])
-    return (pile)
+        pile.append(deck[0])
+        deck.remove(deck[0])
+    return (deck,pile)
 
-print(shuffle_deal(Deck,2))
+print(shuffle_deal(Deck,2)[1])
