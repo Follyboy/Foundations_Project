c5a0d8ddf70310b03b85dac3ec3e6e7923c60369
Carrie Magee <mageec@gwu.edu>
2023-11-14 16:47:18 -0500

    added scoring function

--- Blackjack.py
+++ Blackjack.py
@@ -21,3 +21,23 @@ def shuffle_deal(deck, n):
     return (deck,pile)
 
 print(shuffle_deal(Deck,2)[1])
+
+#This function calculates the current score of the hand; since A can be 1 or 11 the function will change depending on the current situation
+def score(cards_input): #input expected should be a a list of numbers and/or strings with a length of 2 or more? depends on how we format other functions I guess 
+    score = 0
+    aces = 0
+
+    for card in cards_input:
+        if card == "A":
+            aces+=1
+            score+=11 #start out with ace == 11 but is adjusted if needed 
+        elif card in ['J','Q','K']:
+            score += 10
+        else:
+            score +=card
+
+    while aces > 0 and score > 21: ##this makes sure that the ace value is changed from 11 to 1 when the score gets too high 
+        score -= 10
+        aces -=1
+
+    return score
